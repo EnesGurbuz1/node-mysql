29035e3d5dc7ae069f627e7602bfc99dad9034b5
Enes Buğra Gürbüz <enesbugragurbuz1@gmail.com>
2023-07-08 15:06:10 +0300

    update crud functions

--- index.js
+++ index.js
@@ -1,88 +1,42 @@
 const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
-const mysql = require('mysql');
+const { ApolloServer} = require('apollo-server-express');
+const resolvers = require('./resolvers.js');
+const typeDefs = require('./schema')
+const db = require('./db.js');
+
 
 const app = express();
 
-const typeDefs = gql`
-  type User {
-    id: Int
-    name: String
-    email: String
-  }
 
-  type Query {
-    getUser(id: Int): User
-    getUsers: [User]
-  }
-`;
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Enes2002',
-  database: 'basecamp'
-});
+// Çözümleyiciler (resolvers) tanımı
 
-const resolvers = {
-  Query: {
-    getUser: ({ id }) => {
-      if (typeof id !== 'number') {
-        throw new Error('Geçerli bir kullanıcı kimliği sağlanmalıdır.');
-      }
-      return new Promise((resolve, reject) => {
-        const query = `SELECT * FROM users WHERE id = ${id}`;
-        db.query(query, (err, results) => {
-          if (err) {
-            reject(err);
-          } else {
-            if (results.length > 0) {
-              const user = {
-                id: results[0].id,
-                name: results[0].name,
-                email: results[0].email
-              };
-              resolve(user);
-            } else {
-              resolve(null);
-            }
-          }
-        });
-      });
-    },
-    getUsers: () => {
-      return new Promise((resolve, reject) => {
-        const query = 'SELECT * FROM users';
-        db.query(query, (err, results) => {
-          if (err) {
-            reject(err);
-          } else {
-            const users = results.map(result => ({
-              id: result.id,
-              name: result.name,
-              email: result.email
-            }));
-            resolve(users);
-          }
-        });
-      });
-    }
-  }
-};
 
+// Apollo Server tanımı
 const server = new ApolloServer({
   typeDefs,
   resolvers
 });
 
-async function startApolloServer() {
-  await server.start();
-  server.applyMiddleware({ app });
-}
 
-startApolloServer().then(() => {
-  const port = 3000;
-  app.listen(port, () => {
-    console.log(`GraphQL sunucusu ${port} portunda çalışıyor.`);
-  });
+db.connect((err) => {
+  if (err) {
+    console.error('Veritabanına bağlanırken bir hata oluştu:', err);
+  } else {
+    console.log('Veritabanına başarıyla bağlandı.');
+
+    // Apollo Server'ı başlatan asenkron fonksiyon
+    async function startApolloServer() {
+      await server.start();
+      server.applyMiddleware({ app });
+    }
+
+    // Apollo Server'ı başlat
+    startApolloServer().then(() => {
+      const port = 3000;
+      app.listen(port, () => {
+        console.log(`GraphQL sunucusu ${port} portunda çalışıyor.`);
+      });
+    });
+  }
 });
